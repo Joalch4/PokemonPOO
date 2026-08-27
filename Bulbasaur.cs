9386d89c11ef5932d754d4bc08193c2891633854
Joalch4 <106455570+Joalch4@users.noreply.github.com>
2026-08-26 23:37:17 -0600

    Add comments to Bulbasaur class
    Added comments to clarify Bulbasaur class functionality.

--- Bulbasaur.cs
+++ Bulbasaur.cs
@@ -1,13 +1,22 @@
+// Clase Bulbasaur que hereda de la clase Pokemon
 class Bulbasaur : Pokemon
 {
+    // Atributo propio de Bulbasaur
     private int solarPower;
-    public Bulbasaur(string name, int level, int health, int solarPower, Attack attack) : base(name, level, health, attack)
+
+    // Constructor de Bulbasaur
+    // Recibe sus datos y utiliza base para enviarlos al constructor de Pokemon
+    public Bulbasaur(string name, int level, int health, int solarPower, Attack attack)
+        : base(name, level, health, attack)
     {
         this.solarPower = solarPower;
     }
+
+    // Sobrescribe el metodo Attack heredado de Pokemon
     public override void Attack(Pokemon target)
     {
-            Console.WriteLine($"Bulbasaur usa {GetAttack().GetName()}.");
-            target.TakeDamage(GetAttack().GetDamage());
+        // Muestra el ataque utilizado y aplica el daño al Pokemon objetivo
+        Console.WriteLine($"Bulbasaur usa {GetAttack().GetName()}.");
+        target.TakeDamage(GetAttack().GetDamage());
     }
-}   
+}
